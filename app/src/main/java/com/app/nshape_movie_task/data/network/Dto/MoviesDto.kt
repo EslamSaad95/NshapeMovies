c892e21a1bca5d8209b19388634c875ddb75b362
eso202 <eslamsaad45@gmail.com>
2022-12-10 15:46:19 +0200

    add movies Dto

---
+++ app/src/main/java/com/app/nshape_movie_task/data/network/Dto/MoviesDto.kt
@@ -0,0 +1,46 @@
+package com.app.nshape_movie_task.data.network.Dto
+
+
+import com.google.gson.annotations.SerializedName
+
+data class MoviesDto(
+    @SerializedName("page")
+    val page: Int,
+    @SerializedName("results")
+    val results: List<Result>,
+    @SerializedName("total_pages")
+    val totalPages: Int,
+    @SerializedName("total_results")
+    val totalResults: Int
+) {
+    data class Result(
+        @SerializedName("adult")
+        val adult: Boolean,
+        @SerializedName("backdrop_path")
+        val backdropPath: Any,
+        @SerializedName("genre_ids")
+        val genreIds: List<Int>,
+        @SerializedName("id")
+        val id: Int,
+        @SerializedName("original_language")
+        val originalLanguage: String,
+        @SerializedName("original_title")
+        val originalTitle: String,
+        @SerializedName("overview")
+        val overview: String,
+        @SerializedName("popularity")
+        val popularity: Double,
+        @SerializedName("poster_path")
+        val posterPath: Any,
+        @SerializedName("release_date")
+        val releaseDate: String,
+        @SerializedName("title")
+        val title: String,
+        @SerializedName("video")
+        val video: Boolean,
+        @SerializedName("vote_average")
+        val voteAverage: Int,
+        @SerializedName("vote_count")
+        val voteCount: Int
+    )
+}
