aee112ee82d0dc11b4081628ca64d9919844d048
eso202 <eslamsaad45@gmail.com>
2022-12-10 20:37:39 +0200

    add movieDetails Dto - movieDetails getway

--- app/src/main/java/com/app/nshape_movie_task/data/network/ApiService.kt
+++ app/src/main/java/com/app/nshape_movie_task/data/network/ApiService.kt
@@ -1,11 +1,16 @@
 package com.app.nshape_movie_task.data.network
 
+import com.app.nshape_movie_task.data.network.Dto.MovieDetailsDto
 import com.app.nshape_movie_task.data.network.Dto.MoviesDto
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface ApiService {
 
   @GET("discover/movie?primary_release_year=2010&sort_by=vote_average.desc")
   suspend fun getMovies():Response<MoviesDto>
+
+  @GET("movie/{movieId}")
+  suspend fun getMovieDetails(@Path("movieId") movieId:Int):Response<MovieDetailsDto>
 }
