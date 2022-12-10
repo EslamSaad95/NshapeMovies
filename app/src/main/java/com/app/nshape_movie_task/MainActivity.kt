19b042f2172e8d982d171f8a03eeeb95b436ae75
eso202 <eslamsaad45@gmail.com>
2022-12-10 14:50:13 +0200

    first Commit

---
+++ app/src/main/java/com/app/nshape_movie_task/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.app.nshape_movie_task
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
