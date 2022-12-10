885e6b67fea3ca0abe4886de39fa6636f25c4027
eso202 <eslamsaad45@gmail.com>
2022-12-10 16:16:44 +0200

    add moviesFragment - navigation graph

---
+++ app/src/main/java/com/app/nshape_movie_task/presentation/movies/MoviesFragment.kt
@@ -0,0 +1,15 @@
+package com.app.nshape_movie_task.presentation.movies
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.app.nshape_movie_task.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MoviesFragment : Fragment() {
+
+
+}
